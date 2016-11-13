c5e153a06184e6fe633d6177053dfab894f304a3
Zeng Guan Xin <zadays9057@gmail.com>
2016-11-13 19:11:37 +0800

    Update bof_shellcode.c

--- bof_shellcode.c
+++ bof_shellcode.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 
-char name[200];
-char *filename;
-
 int main(){
-	FILE *fp ;
-	char buf[2704];
-	puts("What's your name ?");
+	char buf[100];
+	printf("The buffer of your input %p\n",buf);
+	puts("Can you pwn it ?");
+	printf("Your input : ");
 	fflush(stdout);
-	filename = "/home/bssof/welcome";
-	gets(name);
-	fp = fopen(filename,"r");
-	if(!fp){
-		puts("Error !!");
-		return -1;
-	}else{
-		fread(buf,2704,1,fp);
-	}
-	printf("%s\n",buf);
-	printf("Goodbye ~ %s\n",name);
+	gets(buf);
+	puts("Goodbye ~");
 	fflush(stdout);
-	return 0 ;
+
 }
